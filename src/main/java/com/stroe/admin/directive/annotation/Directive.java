cfaf6b2d6993bd0a026bf83f073b7c0e003f1a43
taoge <ADMIN@DESKTOP-2Q8KPMH>
2017-10-23 09:54:57 +0800

    。

---
+++ src/main/java/com/stroe/admin/directive/annotation/Directive.java
@@ -0,0 +1,22 @@
+package com.stroe.admin.directive.annotation;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * 模板标签注解
+ * @author zengjintao
+ * @version 1.0
+ * @create_at 2017年9月18日下午10:34:03
+ */
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.RUNTIME)
+@Documented
+public @interface Directive {
+
+	String name();
+}
+
