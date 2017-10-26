94526dbf8b5663e47878882ebe5782cc9f927ae5
taoge <ADMIN@DESKTOP-2Q8KPMH>
2017-10-26 19:44:36 +0800

    修改

--- src/main/java/com/stroe/admin/annotation/Bean.java
+++ src/main/java/com/stroe/admin/annotation/Bean.java
@@ -5,15 +5,9 @@ import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
-
-/**
- * aop自动注入注解
- * @author zengjintao
- * @version 1.0
- * @create_at 2017年7月24日下午9:21:12
- */
 @Retention(RetentionPolicy.RUNTIME)
-@Target({ElementType.FIELD})
+@Target({ElementType.FIELD,ElementType.TYPE})
 @Documented
-public @interface AopBean {
+public @interface Bean {
+	String value() default "";
 }
--- src/main/java/com/stroe/admin/annotation/Inject.java
+++ src/main/java/com/stroe/admin/annotation/Inject.java
@@ -5,9 +5,15 @@ import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
+
+/**
+ * aop自动注入注解
+ * @author zengjintao
+ * @version 1.0
+ * @create_at 2017年7月24日下午9:21:12
+ */
 @Retention(RetentionPolicy.RUNTIME)
-@Target({ElementType.FIELD,ElementType.TYPE})
+@Target({ElementType.FIELD})
 @Documented
-public @interface Aop {
-	String value() default "";
+public @interface Inject {
 }
